ed754fa5395f25053ff1e43f9074780693252dd6
Anders Arnholm <anders@arnholm.se>
2020-02-16 08:55:42 +0100

    Make CI-Build one step.
    CMake always use absolute path, Concourse likes to change root folder.

---
+++ ci_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euxo pipefail
+
+cmake -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug ../ets2_telemetry_mqtt
+cmake --build . --target ets2telemery
+cmake --build . --target ets2clienttest -- -v
+ctest .
