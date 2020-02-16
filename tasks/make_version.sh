57f59d92d8a28df000c3bfec4839753de36ddebe
Anders Arnholm <anders@arnholm.se>
2020-02-16 15:32:41 +0100

    Add personal version of treeder/bump with git command

--- tasks/make_version.sh
+++ tasks/make_version.sh
@@ -6,13 +6,13 @@ ls -la ..
 ls -la ../html_docs
 ls -la ../ets2_telemetry_mqtt
 
-(cd ../ets2_telemetry_mqtt ; git describe --tags --always) > name
-#echo "Test v0.0.0" > name
-echo v0.0.0 > tag
+(cd ../ets2_telemetry_mqtt ; /script/bump --input "$(git describe --tags --always)" "$(git log -1 --pretty=%B)" ) > name
+(cd ../ets2_telemetry_mqtt ; /script/bump --input "$(git describe --tags --always)" "$(git log -1 --pretty=%B)" ) > tag
 (cd ../ets2_telemetry_mqtt ; git log --pretty=format:"%h - %an, %ar : %s" --since=2.weeks) > body
-# echo "Release info goes here!!!" > body
 mkdir -p files
 tar c -C ../html_docs -j -f files/docs.tar.bz2 .
 cp ../build_alpine_clang/libets2telemery.so files/
+
+/script/bump --input "$(git describe --tags --always)" --extraxt
 
 find .
