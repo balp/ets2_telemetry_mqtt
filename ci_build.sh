34fcdfa8dbe0d3e2b46ce0b395a0c07193c0df22
Anders Arnholm <anders@arnholm.se>
2020-02-16 11:49:23 +0100

    Split build, unittest and doc jobs

--- ci_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eux
-
-cmake -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug ../ets2_telemetry_mqtt
-cmake --build . --target ets2telemery
-cmake --build . --target ets2clienttest -- -v
-ctest .
