9c428b7b1364adf4f0656f95d22891bd538fd93c
Anders Arnholm <anders@arnholm.se>
2020-02-16 09:46:21 +0100

    Split task and add compiler-rt to Alpine

--- ci_build.sh
+++ ci_build.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -euxo pipefail
+#!/bin/sh
+set -eux
 
 cmake -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug ../ets2_telemetry_mqtt
 cmake --build . --target ets2telemery
